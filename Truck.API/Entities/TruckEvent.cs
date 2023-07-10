cc1eb8a4fb1b96698ffd7f0bad68c65945cb8764
flemos <ferlemos92@gmail.com>
2023-07-10 14:13:20 -0300

    convert namespace in global scope

--- Truck.API/Entities/TruckEvent.cs
+++ Truck.API/Entities/TruckEvent.cs
@@ -1,37 +1,36 @@
-﻿namespace Truck.API.Entities
+﻿namespace Truck.API.Entities;
+
+public class TruckEvent
 {
-    public class TruckEvent
+
+    public TruckEvent()
     {
-
-        public TruckEvent()
-        {
-            isDeleted = false;
-        }
-
-
-        public Guid Id { get; set; }
-
-        public string Brand { get; set; }
-        public string Model { get; set; }
-
-        public int ManafactureYear { get; set; }
-
-        public int ModelYear { get; set; }
-
-        public bool isDeleted { get; set; }
-
-        public void Update(string model, string brand, int manafactureyear, int modelyear)
-        {
-            Model = model;
-            Brand = brand;
-            ManafactureYear = manafactureyear;
-            ModelYear = modelyear;
-        }
-
-        public void Delete()
-        {
-            isDeleted = true;
-        }
-
+        isDeleted = false;
     }
+
+
+    public Guid Id { get; set; }
+
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public int ManafactureYear { get; set; }
+
+    public int ModelYear { get; set; }
+
+    public bool isDeleted { get; set; }
+
+    public void Update(string model, string brand, int manafactureyear, int modelyear)
+    {
+        Model = model;
+        Brand = brand;
+        ManafactureYear = manafactureyear;
+        ModelYear = modelyear;
+    }
+
+    public void Delete()
+    {
+        isDeleted = true;
+    }
+
 }
